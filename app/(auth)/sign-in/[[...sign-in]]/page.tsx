487bd2157c38d483407491940df395a94b82840c
Tiago Lima <tiagolima@osf.digital>
2026-05-27 17:55:05 -0300

    chore: Removed the unnecessary sign in and sign up router pages.

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function SignInPage() {
-  return (
-    <div className="flex w-full h-screen items-center justify-center">
-      <SignIn />
-    </div>
-  );
-}
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function SignUpPage() {
-  return (
-    <div className="flex w-full h-screen items-center justify-center">
-      <SignUp />
-    </div>
-  );
-}
