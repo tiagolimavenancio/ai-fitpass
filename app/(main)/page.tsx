135ffdd91620986ba6d2e00c45d4c5ce9b5d0cf4
Tiago Lima <tiagolima@osf.digital>
2026-05-27 15:03:39 -0300

    chore: Added the shadcn components.

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-	return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div>
+        <Button variant="default">Click Me</Button>
+      </div>
+    </div>
+  );
 }
